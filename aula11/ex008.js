c09e95d1c1684736fcf764bd16846426028309cc
DouglaSchmitzz <douglaschmitzz@gmail.com>
2024-02-16 01:17:54 +0000

    condições em js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 40;
+console.log(`A velocidade do seu carro é ${vel}km/hr`);
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade limite. Multado!`);
+}
+console.log(`Dirija sempre com cinto de segurança`)
