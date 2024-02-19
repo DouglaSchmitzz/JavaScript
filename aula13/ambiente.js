ae9ddd1fc2c554324bf00a3c7a4025c837cafda2
DouglaSchmitzz <douglaschmitzz@gmail.com>
2024-02-19 21:45:44 +0000

    repetições e debug

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*let c = 1;
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 6);*/
+
+/* let c = 1;
+   while (c <= 6) {
+    console.log(`Passo ${c}`);
+    c++;
+}*/
+
+console.log("Vai começar...");
+for (let c = 1; c <= 4; c++) {
+  console.log(c);
+}
+console.log("fim!");
